e60938787e3297937d836f273d98459ffd4ca419
dineth07github <155878601+dineth07github@users.noreply.github.com>
2025-08-10 22:26:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,13 +11,13 @@ AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY DARKZONE-MD 🤍*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY © 𝔻𝕚𝕟𝕦 𝕄𝔻 🤍*",
 // set the auto reply massage on status reply  
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
 // set true false for anti delete     
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", 
 // change it to 'same' if you want to resend deleted message in same chat     
 WELCOME: process.env.WELCOME || "false",
 // true if want welcome and goodbye msg in groups    
@@ -27,33 +27,33 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/4964gx.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://yt3.googleusercontent.com/z2w0NyQKv9HTqytZ6yH136RJ5hBnsOxh4QQw_cCH6FKMoYiu2SlFsnEB-Ic87XFuaFPPPyM_QQ=s176-c-k-c0x00ffffff-no-rj-mo",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "DARKZONE-MD",
+BOT_NAME: process.env.BOT_NAME || "© 𝔻𝕚𝕟𝕦 𝕄𝔻",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "DARKZONE-MD",
+STICKER_NAME: process.env.STICKER_NAME || "© 𝔻𝕚𝕟𝕦 𝕄𝔻",
 // type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "🧙‍♂️",
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923306137477",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94785602293",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟",
+OWNER_NAME: process.env.OWNER_NAME || "© 𝔻𝕚𝕟𝕦 𝕄𝔻",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© CREATER 𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟 *",
+DESCRIPTION: process.env.DESCRIPTION || "*© 𝔻𝕚𝕟𝕦 𝕄𝔻*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/4964gx.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://yt3.googleusercontent.com/z2w0NyQKv9HTqytZ6yH136RJ5hBnsOxh4QQw_cCH6FKMoYiu2SlFsnEB-Ic87XFuaFPPPyM_QQ=s176-c-k-c0x00ffffff-no-rj-mo",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> HEY IM ALIVE NOW  *DARKZONE-MD*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> HEY IM ALIVE NOW  *© 𝔻𝕚𝕟𝕦 𝕄𝔻*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
@@ -73,7 +73,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "true",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923306137477",
+DEV: process.env.DEV || "94785602293",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
